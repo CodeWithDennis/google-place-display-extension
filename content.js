a08567dffe6263fe14cff251066a2723d453a930
Dennis Elsinga <personal.elsinga@gmail.com>
2023-05-18 11:46:37 +0200

    Added code comments

--- content.js
+++ content.js
@@ -1,16 +1,26 @@
+// Regular expression to match the Place ID pattern (e.g., ChIJ followed by non-space characters)
 const regex = /ChIJ\S*/g;
+
+// Find all elements with a data-pid attribute starting with "ChIJ"
 const elements = document.querySelectorAll('[data-pid^="ChIJ"]');
+
+// Variable to store the extracted Place ID
 let place = null;
 
+// Iterate through the elements
 for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
     const pid = element.getAttribute('data-pid');
 
+    // Check if the Place ID matches the regex pattern
     if (regex.test(pid)) {
+        // Store the matched Place ID
         place = pid;
+        // Exit the loop since we found a matching Place ID
         break;
     }
 }
+
 // Find the first element with the ID "kp-wp-tab-overview"
 let firstElement = document.querySelector("#kp-wp-tab-overview");
 
@@ -63,6 +73,9 @@ function copyToClipboard(text) {
     document.body.removeChild(input);
 }
 
+// Check if a valid Place ID was found
 if (place) {
-    chrome.runtime.sendMessage({place: '1'});
+    // Send a message to the background script or event page
+    // indicating that a valid Place ID was found
+    chrome.runtime.sendMessage({ place: '1' });
 }
